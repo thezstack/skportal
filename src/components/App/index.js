ccb1ce7ee5ca253b3ed8c957d4e607e6144a9a6a
Zubair Mughal <zmu@equinor.com>
2019-09-03 14:39:42 -0500

    firebase hookup and env

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import Navigation from '../Navigation';
 import LandingPage from '../Landing';
 import SignUpPage from '../SignUp';
@@ -14,13 +14,14 @@ const App = () => (
      <div>
       <Navigation />
       <hr />
-      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route exact path={ROUTES.LANDING} component={LandingPage} /> 
       <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
       <Route path={ROUTES.SIGN_IN} component={SignInPage} />
       <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
       <Route path={ROUTES.HOME} component={HomePage} />
       <Route path={ROUTES.ACCOUNT} component={AccountPage} />
       <Route path={ROUTES.ADMIN} component={AdminPage} />  
+  
     </div> 
   </Router>
 
