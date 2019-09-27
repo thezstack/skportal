38c905066cedaa305d3573a57299265fcedd7efc
Zubair Mughal <mghlzbr@gmail.com>
2019-09-27 17:41:22 -0500

    got react table to finally work. have description, price and product image working

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { withFirebase } from "../Firebase";
+import ReactTable from 'react-table'
+import 'react-table/react-table.css'
 
 class Home extends Component {
   constructor(props) {
@@ -19,7 +21,9 @@ class Home extends Component {
         loading: false
       });
 
+
     });
+    
 
  
 
@@ -33,15 +37,56 @@ class Home extends Component {
   render() {
     const { inventory, loading, childData } = this.state;
 
+    let selectedPerson= [];
+for (let person of inventory) {
+  console.log();
+  let counter= 0;
+  selectedPerson.push(person[0]);
+  selectedPerson.push(person[5]);
+  counter++;
+  
+}
+
+
+console.log(selectedPerson);
+
+
+
     return (
       <div>
         <h1>Home</h1>
         {loading && <div>Loading ...</div>}
-       <InventoryList inventory={inventory}/>
+        <ReactTable
+  data={inventory}
+  columns={columns}
+  filterable
+/> 
       </div>
     );
   }
 }
+const columns = [ {
+  id:d => d[5],
+  Header: 'Product Description',
+  accessor: d => d[0],
+  filterAll: false
+
+},
+{
+  id:'Price',
+  Header:'Price',
+  accessor: a => '$'+ a[7],
+  filterable:false
+},
+{
+  id:'Image',
+  Header:'Product Image',
+  accessor:b => <img width = '100' src={b[8]} alt='yo' />,
+  filterable:false
+}
+
+]
+
 
 const InventoryList = ({ inventory }) => (
   <ul>
