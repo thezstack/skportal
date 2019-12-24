a7a0aafa4f4f56615912fd70972fd185ef48f9ee
Zubair Mughal <mghlzbr@gmail.com>
2019-12-24 12:11:57 -0600

    ran npm audit

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -9,6 +9,7 @@ const config = {
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId:process.env.REACT_APP_APP_ID
 };
 
 class Firebase {
@@ -87,8 +88,8 @@ class Firebase {
         fallback();
       }
     });
-
-inventory = () => this.db.ref('masterSheet').orderByKey();
+  //grabing inventory
+  inventory = () => this.db.ref('masterSheet').orderByKey();
 
   // *** User API ***
 
