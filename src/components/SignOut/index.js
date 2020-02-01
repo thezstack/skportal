13e69ed4bb08202902a2e75d121a2224d0559563
Zubair Mughal <mghlzbr@gmail.com>
2020-02-01 13:27:29 -0600

    got signout redirect to start working

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import {withFirebase} from '../Firebase'
+import * as ROUTES from '../../constants/routes';
 
-const SignOutButton = ({firebase}) => (
-    <button type="button" onClick={firebase.doSignOut}> Sign Out </button>
-);
 
-export default withFirebase (SignOutButton);
+class SignOut extends Component {
+
+    onClick = event =>{
+        this.props.firebase.doSignOut();
+        this.props.history.push(ROUTES.LANDING);
+    }
+
+render(){
+    return(
+        <button type="button" onClick={this.onClick}> Sign Out </button>
+    )
+}
+}
+
+export default withRouter(withFirebase (SignOut));
