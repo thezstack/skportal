fd94b78050b6fc9f42dbeec47444c79b495219ef
Zubair Mughal <mghlzbr@gmail.com>
2020-02-01 17:59:26 -0600

    updated some styling on sign in page

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,80 +1,109 @@
-import React, { Component } from 'react';
-import {withRouter} from 'react-router-dom';
-import {compose} from 'recompose';
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { compose } from "recompose";
 
-import {SignUpLink} from '../SignUp';
-import {withFirebase} from '../Firebase';
-import * as ROUTES from '../../constants/routes'
+import { SignUpLink } from "../SignUp";
+import { withFirebase } from "../Firebase";
+import * as ROUTES from "../../constants/routes";
 
 const SignInPage = () => (
-<div>
-    <h1>Sign In</h1>
-    <SignInForm />
+  <div>
+    <div className="container">
+    <h1>Welcome to School Kits Portal</h1>
     <SignUpLink />
-</div>
+    <SignInForm />
+ 
+
+    </div>
+
+
+    <style jsx>
+      {`
+        .container {
+          background-color: #ccfbfe;
+          display:flex;
+          justify-content:center;
+          flex-direction:column;
+        
+        }
+      `}
+    </style>
+  </div>
 );
 
 const INITIAL_STATE = {
-    email:'',
-    password:'',
-    error: null
-}
+  email: "",
+  password: "",
+  error: null
+};
 
-class SignInFormBase extends Component{
-    constructor(props){
-        super(props);
-        this.state ={...INITIAL_STATE}         
-    }
-    onSubmit = event => {
-        const {email, password} = this.state;
-        this.props.firebase
-        .doSignInWithEmailAndPassword(email, password)
-        .then(() => {
-            this.setState({...INITIAL_STATE});
-            this.props.history.push(ROUTES.HOME);
+class SignInFormBase extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...INITIAL_STATE };
+  }
+  onSubmit = event => {
+    const { email, password } = this.state;
+    this.props.firebase
+      .doSignInWithEmailAndPassword(email, password)
+      .then(() => {
+        this.setState({ ...INITIAL_STATE });
+        this.props.history.push(ROUTES.HOME);
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+    event.preventDefault();
+  };
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-        })
-        .catch(error => {
-            this.setState({error})
-        });
-        event.preventDefault();
-        
-    };
-    onChange = event =>{
-        this.setState({[event.target.name]: event.target.value});
-    };
+  render() {
+    const { email, password, error } = this.state;
+    const isInvalid = password === " " || email === "";
 
-    render(){
-        const {email, password, error} = this.state;
-        const isInvalid = password === ' ' || email === '';
-
-        return(
-        <div>
-            <form onSubmit={this.onSubmit}>
-            <input name="email" value={email} onChange={this.onChange} type="text" placeholder="Email Address" />
-            <input name="password" value ={password} onChange={this.onChange} type ="password" placeholder="Password" />
-            <button disabled={isInvalid} type="submit" > Sign In</button>
+    return (
+      <div>
+        <div >
+          <form onSubmit={this.onSubmit}>
+            <input
+              name="email"
+              value={email}
+              onChange={this.onChange}
+              type="text"
+              placeholder="Email Address"
+            />
+            <input
+              name="password"
+              value={password}
+              onChange={this.onChange}
+              type="password"
+              placeholder="Password"
+            />
+            <button disabled={isInvalid} type="submit">
+              Sign In
+            </button>
             {error && <p>{error.message}</p>}
+          </form>
+        </div>
+        <style jsx>
+          {`
+            form{
+              background-color: white;
+              display:flex;
+              flex-direction:column;
+              justify-content:center;
+            
+              align-content:center;
+            }
 
-            </form>
-            <style jsx>
-                {`
-
-                form{
-                    background-color: red;
-                }
-                
-                `}
-
-            </style>
-            </div>
-        )
-
-    }
+          `}
+        </style>
+      </div>
+    );
+  }
 }
-const SignInForm = compose(
-    withRouter,
-    withFirebase,
-  )(SignInFormBase);
-  export default SignInPage;
-  export { SignInForm };
+const SignInForm = compose(withRouter, withFirebase)(SignInFormBase);
+export default SignInPage;
+export { SignInForm };
